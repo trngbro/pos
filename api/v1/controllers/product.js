8b70baea37bbc917e7cf310dc3d1e79853e39ec6
Phuc <phuctran12421@gmail.com>
2023-10-24 17:43:21 +0700

    get products for productTable

--- api/v1/controllers/product.js
+++ api/v1/controllers/product.js
@@ -6,16 +6,38 @@ const scripts = require('../helpers/javascriptConfig')
 const productController = {
     viewAllProducts: async (req, res) => {
         try {
-            const products = await Products.find({}).exec();
-            console.log(typeof(JSON.stringify(products)))
+            let products = await Products.find({}).exec();
+            let arr = []
+
+            products.forEach(element => {
+                let date = new Date(element.dateCreated)
+                const day = date.getDate();
+                const month = date.getMonth() + 1;
+                const year = date.getFullYear();
+                const formattedDate = `${day}/${month}/${year}`;
+
+                console.log(formattedDate);
+                arr.push({
+                    barcode: element.barcode,
+                    name: element.name,
+                    images: element.images,
+                    originalPrice: element.originalPrice,
+                    dateCreated: formattedDate,
+                    salePrice: element.salePrice,
+                    categoryName: element.categoryName,
+                    qty: element.qty,
+                    sold: element.sold,
+                })
+            });
+
             res.render("productTable", {
                 pathIsLevelTwo: false,
                 stylesheets: styles.tableCSS,
                 javascripts: scripts.tableJS,
-                productTableData: (products)
+                productTableData: arr
             });
         } catch (error) {
-          res.render("error");
+            res.render("error");
         }
     },
     viewCreateProduct: (req, res) => {
