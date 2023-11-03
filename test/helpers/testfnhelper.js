00b6b1949c9867bed44d242ffe78539925ef4db1
Vũ Trường Giang <52000656@student.tdtu.edu.vn>
2023-11-03 17:14:58 +0000

    Upload New File

---
+++ test/helpers/testfnhelper.js
@@ -0,0 +1,73 @@
+const assert = require('assert');
+const { formatCurrency, getStaffNameFromSalerData, toUnicodeEscapedString, fromUnicodeEscapedString, equalStringWithString } = require('../../test/helpers/functionalHelper');
+// Kiểm tra hàm formatCurrency
+describe('formatCurrency', function () {
+    it('should format a number as currency', function () {
+        const formattedValue = formatCurrency(1000000);
+        assert.strictEqual(formattedValue, '1.000.000 ₫');
+    });
+
+    it('should convert and format a string as currency', function () {
+        const formattedValue = formatCurrency('1500000');
+        assert.strictEqual(formattedValue, '1.500.000 ₫');
+    });
+
+    it('should return the same value if it is not a number or a string', function () {
+        const value = { prop: 'value' };
+        const result = formatCurrency(value);
+        assert.strictEqual(result, value);
+    });
+});
+
+// Kiểm tra hàm getStaffNameFromSalerData
+describe('getStaffNameFromSalerData', function () {
+    it('should extract staff name from a string with "<<>>"', function () {
+        const staffName = getStaffNameFromSalerData('John<<>>Doe');
+        assert.strictEqual(staffName, 'John');
+    });
+
+    it('should return a default value if the input is not a string', function () {
+        const result = getStaffNameFromSalerData(12345);
+        assert.strictEqual(result, 'name');
+    });
+});
+
+// Kiểm tra hàm toUnicodeEscapedString và fromUnicodeEscapedString
+describe('Unicode Escaped String', function () {
+    it('should convert a string to a Unicode-escaped string', function () {
+        const escapedString = toUnicodeEscapedString('Hello');
+        assert.strictEqual(escapedString, '\\u0048\\u0065\\u006c\\u006c\\u006f');
+    });
+
+    it('should convert a Unicode-escaped string back to the original string', function () {
+        const originalString = fromUnicodeEscapedString('\\u0048\\u0065\\u006c\\u006c\\u006f');
+        assert.strictEqual(originalString, 'Hello');
+    });
+});
+
+// Kiểm tra hàm equalStringWithString
+describe('equalStringWithString', function () {
+    it('should call the true block if two strings are equal', function () {
+        const result = equalStringWithString('abc', 'abc', {
+            fn: function () {
+                return 'true block';
+            },
+            inverse: function () {
+                return 'false block';
+            },
+        });
+        assert.strictEqual(result, 'true block');
+    });
+
+    it('should call the false block if two strings are not equal', function () {
+        const result = equalStringWithString('abc', 'def', {
+            fn: function () {
+                return 'true block';
+            },
+            inverse: function () {
+                return 'false block';
+            },
+        });
+        assert.strictEqual(result, 'false block');
+    });
+});
