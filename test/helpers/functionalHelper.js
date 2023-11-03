d977ad776d8e2fb8e4023506e879fc3ca0591bbf
Vũ Trường Giang <52000656@student.tdtu.edu.vn>
2023-11-03 17:14:44 +0000

    Upload New File

---
+++ test/helpers/functionalHelper.js
@@ -0,0 +1,37 @@
+module.exports = {
+    formatCurrency: function (value) {
+        if (typeof value === 'string') {
+            value = parseFloat(value);
+        }
+        if (typeof value === 'number') {
+            return value.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+        } else {
+            return value;
+        }
+    },
+
+    getStaffNameFromSalerData: function (value) {
+        if (typeof value === 'string') {
+            return value.split("<<>>")[0];;
+        }
+        return "name";
+    },
+
+    toUnicodeEscapedString: function(inputString){
+        return inputString.split('').map(char => {
+            const unicodeValue = char.charCodeAt(0).toString(16).toUpperCase();
+            return `\\u${'0'.repeat(4 - unicodeValue.length)}${unicodeValue}`;
+        }).join('');
+    },
+
+    fromUnicodeEscapedString: function(unicodeEscapedString){
+        return unicodeEscapedString.replace(/\\u[\dA-Fa-f]{4}/g, match => {
+            const unicodeValue = parseInt(match.substr(2), 16);
+            return String.fromCharCode(unicodeValue);
+        });
+    },
+
+    equalStringWithString: function(a, b, options){
+        return a === b ? options.fn(this) : options.inverse(this);
+    }
+};
