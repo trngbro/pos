4546e0a327fa52bb78a101b344f2c68a4a9e15e5
Nguyễn Trung Nghĩa <87295854+trngbro@users.noreply.github.com>
2023-11-23 17:35:16 +0700

    genarate .env

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+(
+    echo PORT=3334
+    echo DATABASE=mongodb+srv://trngx:nguyentrungnghia@nodejs.tt447fk.mongodb.net/?retryWrites=true^&w=majority
+    echo NODE_NO_WARNINGS=1
+    echo HOST=smtp.gmail.com
+    echo USER=noreply.trngx@gmail.com
+    echo PASS=zocbrwrghibvmzux
+    echo SERVICE=Gmail
+    echo BASE_URL=http://localhost:3334
+) > .env
